0ade45fec044008b5efe4a8b1de6548746c87cf8
ghost-hax <ghostemblem08@gmail.com>
2022-02-04 16:06:31 +0000

    unit testing

---
+++ MoviesTests/MockCoreDataRepository.swift
@@ -0,0 +1,8 @@
+//
+//  MockCoreDataRepository.swift
+//  MoviesTests
+//
+//  Created by David on 04/02/2022.
+//
+
+import Foundation
---
+++ MoviesTests/MockNetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  MockNetworkManager.swift
+//  MoviesTests
+//
+//  Created by David on 04/02/2022.
+//
+
+import Foundation
